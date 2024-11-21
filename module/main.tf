363d07dcb4be6c66ce87ac4ede9b0accbfb3960d
Harshita <harshitabhadimane02051998@gmail.com>
2024-11-21 16:31:31 +0530

    Creating role for prometheus

--- module/main.tf
+++ module/main.tf
@@ -2,8 +2,32 @@ resource "aws_instance" "instance" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.selected.id]
+  #iam_instance_profile  = ""
 
   tags = {
     Name = var.tool_name
+  }
+}
+
+resource "aws_iam_role" "test_role" {
+  name = "${var.tool_name}-role"
+
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name = "${var.tool_name}-role"
   }
 }
